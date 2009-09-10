bbd1d30e5bce0ec32f4adedfbbecae33a6e053af
Linus Nilsson <lajnold at acc.umu.se>
2009-09-10 14:10:41 +0200

    Merge the stuff in parse_size.c into options.c.

--- src/options.c
+++ src/options.c
@@ -1,14 +1,71 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <limits.h>
+#include <errno.h>
+#include <strings.h>
 
 #include "options.h"
-#include "parse_size.h"
 
 struct fallocate_options fallocate_options = {
 	.byte_multiplier = 1024,
 	.force = 0,
 	.help = 0
 };
+
+static long long checked_multiply(long long start, int times)
+{
+	int multiplier = fallocate_options.byte_multiplier;
+
+	while (times--) {
+		if (start > LLONG_MAX / multiplier)
+			return -1;
+		else
+			start *= multiplier;
+	}
+
+	return start;
+}
+
+static long long parse_size(const char *str)
+{
+	char *ptr;
+	long long size;
+
+	errno = 0;
+	size = strtoll(str, &ptr, 0);
+
+	if (errno == ERANGE || size < 0) {
+		fprintf(stderr,
+			"Size out of range; it must be within 0 and %lld bytes.\n",
+			LLONG_MAX);
+		return -1;
+	}
+
+	if (errno || ptr == str) {
+		fprintf(stderr, "Invalid size specified.\n");
+		return -1;
+	}
+
+	if (!strcasecmp(ptr, "k"))
+		size = checked_multiply(size, 1);
+	else if (!strcasecmp(ptr, "m"))
+		size = checked_multiply(size, 2);
+	else if (!strcasecmp(ptr, "g"))
+		size = checked_multiply(size, 3);
+	else if (*ptr) {
+		fprintf(stderr, "Invalid suffix \"%s\" specified.\n", ptr);
+		return -1;
+	}
+
+	if (size == -1) {
+		fprintf(stderr,
+			"Size out of range; it must be within 0 and %lld bytes.\n",
+			LLONG_MAX);
+		return -1;
+	}
+
+	return size;
+}
 
 int parse_options(int argc, char *argv[])
 {
--- src/parse_size.h
+++
@@ -1,6 +0,0 @@
-#ifndef FALLOCATE_PARSE_H
-#define FALLOCATE_PARSE_H
-
-long long parse_size(const char *str);
-
-#endif
